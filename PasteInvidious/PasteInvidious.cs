80c74c877d68f51b1e7c1bfd86ece43b70836c66
art0007i <art0007i@gmail.com>
2021-10-24 16:31:26 +0200

    Moved to import dialog instead of forcing invidious always

--- PasteInvidious/PasteInvidious.cs
+++ PasteInvidious/PasteInvidious.cs
@@ -8,6 +8,7 @@ using System.Collections.Generic;
 using FrooxEngine;
 using FrooxEngine.LogiX;
 using CodeX;
+using FrooxEngine.UIX;
 
 namespace PasteInvidious
 {
@@ -15,7 +16,7 @@ namespace PasteInvidious
     {
         public override string Name => "PasteInvidious";
         public override string Author => "art0007i";
-        public override string Version => "1.0.1";
+        public override string Version => "1.1.0";
         public override string Link => "https://github.com/art0007i/PasteInvidious/";
         public override void OnEngineInit()
         {
@@ -24,31 +25,46 @@ namespace PasteInvidious
 
         }
 
-        [HarmonyPatch(typeof(UniversalImporter))]
-        [HarmonyPatch("ImportTask")]
-        class UniversalImporter_ImportTask_Patch
+        [HarmonyPatch(typeof(VideoImportDialog))]
+        [HarmonyPatch("OpenRoot")]
+        class VideoImportDialog_OpenRoot_Patch
         {
             public const string InvidiousInstance = "invidious-us.kavin.rocks";
 
-            public static void Prefix(AssetClass assetClass, ref IEnumerable<string> files)
+            public static void Postfix(VideoImportDialog __instance, UIBuilder ui)
             {
-                List<string> newList = new List<string>();
-                foreach (string file in files)
+                bool paths = __instance.Paths.Where(str =>
                 {
-                    string item = file;
                     Uri uri;
-                    if (assetClass == AssetClass.Video && Uri.TryCreate(file, UriKind.Absolute, out uri))
-                    {
-                        if (uri.Host.Contains("youtube.com"))
-                        {
-                            item = file.Replace(uri.Host, InvidiousInstance) + "&raw=1";
-                        }else if (uri.Host.Contains("youtu.be")){
-                            item = file.Replace(uri.Host + "/", InvidiousInstance + "/watch?v=") + "&raw=1";
-                        }
-                    }
-                    newList.Add(item);
+                    if (!Uri.TryCreate(str, UriKind.Absolute, out uri)) return false;
+                    return uri.Host.Contains(InvidiousInstance);
+                }).Any();
+                if (!paths)
+                {
+                    ui.Button("Import Invidious").LocalPressed += ImportInvidious;
                 }
-                files = newList;
+            }
+
+            private static void ImportInvidious(IButton button, ButtonEventData eventData)
+            {
+                var importer = button.Slot.GetComponentInParents<VideoImportDialog>();
+                if((bool)AccessTools.Property(typeof(ImportDialog), "CanInteract").GetValue(importer))
+                {
+                    importer.Paths = importer.Paths.ConvertAll(str =>
+                    {
+                        Uri uri;
+                        if (Uri.TryCreate(str, UriKind.Absolute, out uri))
+                        {
+                            if (uri.Host.Contains("youtube.com"))
+                                return str.Replace(uri.Host, InvidiousInstance) + "&raw=1&quality=medium";
+                            else if (uri.Host.Contains("youtu.be"))
+                                return str.Replace(uri.Host + "/", InvidiousInstance + "/watch?v=") + "&raw=1&quality=medium";
+                        }
+                        return str;
+                    });
+                    // hmm yes reflection is fun
+                    AccessTools.Method(typeof(ImportDialog), "Open").Invoke(importer, new object[] { (Action<UIBuilder>)AccessTools.Method(typeof(VideoImportDialog), "OpenRoot", new Type[] { typeof(UIBuilder) }).CreateDelegate(typeof(Action<UIBuilder>), importer) });
+                }
             }
         }
     }
